8fa45cfeaa23ca8c5e83053987b8bc440f56f69e
Lyla Fischer <lylafisch@gmail.com>
2015-11-08 19:12:33 -0500

    contextualized the javascript

--- client_side_authoring/static/js/src/provide_services.js
+++ client_side_authoring/static/js/src/provide_services.js
@@ -1,12 +1,9 @@
-// function evalInContext(js, context) {
-//     //# Return the results of the in-line anonymous function we .call with the passed context
-//     return function() { return eval(js); }.call(context);
-// }
+function evalInContext(js, context) {
+    return function() { return eval(js); }.call(context);
+}
 function ProvideServices(runtime, element) {
-    // var services = {save_handler_url: runtime.handlerUrl(element, 'save_html')};
-    // console.log("first draft " + save_handler_url);
-    var save_handler_url = runtime.handlerUrl(element, 'save_student_data');
-    var get_data_handler_url = runtime.handlerUrl(element, 'get_student_data');
-    eval(authored_javascript);
-    // evalInContext(authored_javascript, services);
+    var services = {"save_handler_url" : runtime.handlerUrl(element, 'save_student_data'),
+    				"get_data_handler_url" : runtime.handlerUrl(element, 'get_student_data')}
+	// TODO: make this cleaner. authored_javascript is defined in client_side_authoring.py with string manipulation while using add_javascript
+    evalInContext(authored_javascript, services);
 }
